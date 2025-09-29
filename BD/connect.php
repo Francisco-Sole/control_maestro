0395070681b6a99a0e3649dfc1f27a06af77fcb6
Francisco Sole <srsole30687@gmail.com>
2025-09-29 21:46:52 +0200

    Cambiamos IPS

--- BD/connect.php
+++ BD/connect.php
@@ -1,9 +1,10 @@
 <?php
 
-function new_connection(){
-	include ("ip.php");
+function new_connection()
+{
+	include("ip.php");
 
-	$link = new mysqli($ip, "fsole", "fsole", "modulocliente");
+	$link = new mysqli($ip, "fsole", "fsole", "controlMaestro");
 	$link->set_charset("utf8") or die();
 
 	return $link;
--- controller/on.php
+++ controller/on.php
@@ -1,9 +1,9 @@
 <?php
 
 $request = $_GET["ip"];
-$ip = "10.2.232.18";
+$ip = "127.0.0.1";
 
-$link = new mysqli($ip, "franc", "capsulecorp", "modulocliente");
+$link = new mysqli($ip, "fsole", "fsole", "controlMaestro");
 $link->set_charset("utf8") or die();
 
 $consulta = "SELECT mac FROM equipos WHERE ip = '$request';";
@@ -14,26 +14,25 @@ while ($pos = mysqli_fetch_array($result)) {
 //$mac = $output[intval($index+1)];
 
 
-$mac_array = preg_split('#:#', $mac); 
+$mac_array = preg_split('#:#', $mac);
 $hwaddr = '';
-foreach($mac_array AS $octet){
+foreach ($mac_array as $octet) {
 	$hwaddr .= chr(hexdec($octet));
 }
-    //Magic Packet
+//Magic Packet
 $packet = '';
-for ($i = 1; $i <= 6; $i++){
+for ($i = 1; $i <= 6; $i++) {
 	$packet .= chr(255);
 }
-for ($i = 1; $i <= 16; $i++){
+for ($i = 1; $i <= 16; $i++) {
 	$packet .= $hwaddr;
 }
-    //set up socket
+//set up socket
 $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-if ($sock){
+if ($sock) {
 	$options = socket_set_option($sock, SOL_SOCKET, SO_BROADCAST, true);
-	if ($options >=0){    
+	if ($options >= 0) {
 		$e = socket_sendto($sock, $packet, strlen($packet), 0, "255.255.255.255", 7);
 		socket_close($sock);
-	}    
+	}
 }
-
